af8a0eddb0aa192e73715ba4fad333f756989ce7
Dominik <domi@bambus.me>
2022-09-06 14:22:37 +0200

    Update common.js

--- common.js
+++ common.js
@@ -1,6 +1,6 @@
 export default {
-    get Dispatcher() {return Webpack.getByProps("dispatch");},
-    get Constants() {return Webpack.getByProps("API_HOST", "EndPoints");},
+    get Dispatcher() {return Webpack.getByProps("dispatch", "isDispatching");},
+    get Constants() {return Webpack.getByProps("API_HOST", "Endpoints");},
     get Flux() {return Object.assign({}, ...Webpack.getByProps(["connectStores"], ["useStateFromStores"], ["Store", "Dispatcher"], {bulk: true}));},
     get React() {return Webpack.getByProps("createElement", "useEffect");},
     get ReactDOM() {return Webpack.getByProps("render", "unmountComponentAtNode");}
