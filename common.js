62cffbbffda9906562bf1b67eebce4ab2a525079
Iucism <105166639+Iucism@users.noreply.github.com>
2022-09-06 08:17:45 -0400

    Update common.js

--- common.js
+++ common.js
@@ -1,6 +1,6 @@
 export default {
-    get Dispatcher() {return Webpack.getByProps("dirtyDispatch");},
-    get Constants() {return Webpack.getByProps("API_HOST", "ActionTypes");},
+    get Dispatcher() {return Webpack.getByProps("dispatch");},
+    get Constants() {return Webpack.getByProps("API_HOST", "EndPoints");},
     get Flux() {return Object.assign({}, ...Webpack.getByProps(["connectStores"], ["useStateFromStores"], ["Store", "Dispatcher"], {bulk: true}));},
     get React() {return Webpack.getByProps("createElement", "useEffect");},
     get ReactDOM() {return Webpack.getByProps("render", "unmountComponentAtNode");}
